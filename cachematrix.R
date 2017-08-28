755d303f973e3f8ed1e05d5a4192c6b111f3acd8
DawidDabkowski <dav.dabkowski@gmail.com>
2017-08-28 12:41:44 +0200

    done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are functions required for the programming assigment 2 of the data
+## science specialization course 2. Their purpose is to efficiently compute the 
+## inverse of a matrix in case when matrix does not change often and so its
+## inverse can be stored in memory.
 
-## Write a short comment describing this function
+## Function below makes a matrix of a special type, which stores cache of
+## its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Function below gets an inverse of a matrix of the special type, defined
+## by previous function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
